93611db0e5aaa2518007fa48311b5ac3e348c98f
unknown <yejz0715@gmail.com>
2022-06-19 17:55:04 +0900

    edit radioBox

--- react-study/src/Component/RadioButton.jsx
+++ react-study/src/Component/RadioButton.jsx
@@ -2,37 +2,29 @@ import React from "react";
 import { useState } from "react";
 
 function RadioButton() {
-  const [clickMale, setClickMale] = useState(false);
-  const [clickFemale, setClickFemale] = useState(false);
-  function handleBtnMale() {
-    setClickMale("남자");
-  }
-  function handleBtnFemale() {
-    setClickFemale("여자");
-  }
+  const [Gender, SetGender] = useState("");
+  //const [상태, 상태함수]=useState(초기값);
+  const handleBtnMale = () => {
+    SetGender("남자");
+  };
+  const handleBtnFemale = () => {
+    SetGender("여자");
+  };
+  const handleBtnReset = () => {
+    SetGender("");
+  };
   return (
     <div>
       <p style={{ fontSize: "20px", fontWeight: "bold" }}>Radio Button</p>
-      <p style={{ fontSize: "30px" }}>
-        성별:{clickMale}
-        {clickFemale}
-      </p>
-      <input
-        type="radio"
-        id="male"
-        checked={clickMale}
-        onClick={handleBtnMale}
-      />
+      <p style={{ fontSize: "30px" }}>성별:{Gender}</p>
+      <input type="radio" name="gander" id="male" onClick={handleBtnMale} />
       {/*radio 버튼은 checked true /false에 동작 */}
-      <label htmlFor="male">남자</label>
-      {/*radio버튼의 이름을 label로 만들고 label의 htmlFor 속성과  input의 id를 연결해줌 label '남자'를 클릭해도 버튼 동작됨   */}
-      <input
-        type="radio"
-        id="female"
-        checked={clickFemale}
-        onClick={handleBtnFemale}
-      />
-      <label htmlFor="female">여자</label>
+      <label for="male">남자</label>
+      {/*radio버튼의 이름을 label로 만들고 label의 htmlFor(=for) 속성과  input의 id를 연결해줌 label '남자'를 클릭해도 버튼 동작됨./ 속성 name을 추가해줘야 하나만 선택이 됨   */}
+      <input type="radio" name="gander" id="female" onClick={handleBtnFemale} />
+      <label for="female">여자</label>
+      <input type="radio" name="gander" id="reset" onClick={handleBtnReset} />
+      <label htmlFor="reset">초기화</label>
     </div>
   );
 }
