824744ebf143cfc849d1ef9437c839f9bfc72f25
unknown <yejz0715@gmail.com>
2022-06-22 00:35:41 +0900

    htmlFor 수정

--- react-study/src/Component/RadioButton.jsx
+++ react-study/src/Component/RadioButton.jsx
@@ -19,10 +19,10 @@ function RadioButton() {
       <p style={{ fontSize: "30px" }}>성별:{Gender}</p>
       <input type="radio" name="gander" id="male" onClick={handleBtnMale} />
       {/*radio 버튼은 checked true /false에 동작 */}
-      <label for="male">남자</label>
+      <label htmlFor="male">남자</label>
       {/*radio버튼의 이름을 label로 만들고 label의 htmlFor(=for) 속성과  input의 id를 연결해줌 label '남자'를 클릭해도 버튼 동작됨./ 속성 name을 추가해줘야 하나만 선택이 됨   */}
       <input type="radio" name="gander" id="female" onClick={handleBtnFemale} />
-      <label for="female">여자</label>
+      <label htmlFor="female">여자</label>
       <input type="radio" name="gander" id="reset" onClick={handleBtnReset} />
       <label htmlFor="reset">초기화</label>
     </div>
