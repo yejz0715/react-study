b7fd3d633856a8d79f56e00b14b4ba2a9269a3b6
unknown <yejz0715@gmail.com>
2022-06-22 17:22:17 +0900

    Hook_useState 공부

---
+++ react-study/src/Hooks/UseState.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+//1 useState사용
+function UseState() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div>
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>decrement</button>
+    </div>
+  );
+}
+// 2 useState 사용하기 전, class 컴포넌트 사용
+class AppUgly extends React.Component {
+  state = {
+    item: 1,
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div>
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
+}
+export default UseState;
